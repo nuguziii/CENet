0073667214ac990bec0e44507d9a819cae4f3824
CHAEN <nuguziii@naver.com>
2020-05-08 18:10:46 +0900

    utils baseline complete

---
+++ src/utils.py
@@ -0,0 +1,42 @@
+import os
+import logging
+import time
+import torch
+from pathlib import Path
+
+def create_logger(opt, phase='train'):
+    root_output_dir = Path(opt.output_dir)
+    # set up logger
+    if not root_output_dir.exists():
+        print('=> creating {}'.format(root_output_dir))
+        root_output_dir.mkdir()
+
+    final_output_dir = root_output_dir / phase / opt.description
+
+    print('=> creating {}'.format(final_output_dir))
+    final_output_dir.mkdir(parents=True, exist_ok=True)
+
+    time_str = time.strftime('%Y-%m-%d-%H-%M')
+    log_file = '{}_{}_{}.log'.format(opt.description, time_str, phase)
+    final_log_file = final_output_dir / log_file
+    head = '%(asctime)-15s %(message)s'
+    logging.basicConfig(filename=str(final_log_file),
+                        format=head)
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    console = logging.StreamHandler()
+    logging.getLogger('').addHandler(console)
+
+    tensorboard_log_dir = Path(opt.log_dir) / (opt.description + '_' + time_str)
+
+    print('=> creating {}'.format(tensorboard_log_dir))
+    tensorboard_log_dir.mkdir(parents=True, exist_ok=True)
+
+    return logger, str(final_output_dir), str(tensorboard_log_dir)
+
+def save_checkpoint(states, is_best, output_dir,
+                    filename='checkpoint.pth'):
+    torch.save(states, os.path.join(output_dir, filename))
+    if is_best and 'state_dict' in states:
+        torch.save(states['best_state_dict'],
+                   os.path.join(output_dir, 'model_best.pth'))
