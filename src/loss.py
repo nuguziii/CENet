348b3fbcc09e3eb2644543bec6c93b3f2d123f1e
CHAEN <nuguziii@naver.com>
2020-05-08 18:10:01 +0900

    dice loss, ce loss baseline complete (x debug)

---
+++ src/loss.py
@@ -0,0 +1,69 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+class Loss(nn.Module):
+    def __init__(self, alpha=1, beta=1, class_weights=None):
+        super(Loss, self).__init__()
+        self.alpha = alpha
+        self.beta = beta
+        self.class_weights = class_weights
+
+    def forward(self, output, shape_output, contour_output, gt, contour_gt):
+        loss = dice_loss(gt, output) + self.alpha * dice_loss(gt, shape_output) + \
+               self.beta * ce_loss(contour_gt, contour_output, self.class_weights)
+        return loss
+
+def dice_loss(true, logits, eps=1e-7):
+    """Computes the Sørensen–Dice loss.
+    Note that PyTorch optimizers minimize a loss. In this
+    case, we would like to maximize the dice loss so we
+    return the negated dice loss.
+    Args:
+        true: a tensor of shape [B, 1, H, W].
+        logits: a tensor of shape [B, C, H, W]. Corresponds to
+            the raw output or logits of the model.
+        eps: added to the denominator for numerical stability.
+    Returns:
+        dice_loss: the Sørensen–Dice loss.
+    """
+    num_classes = logits.shape[1]
+    if num_classes == 1:
+        true_1_hot = torch.eye(num_classes + 1)[true.squeeze(1)]
+        true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
+        true_1_hot_f = true_1_hot[:, 0:1, :, :]
+        true_1_hot_s = true_1_hot[:, 1:2, :, :]
+        true_1_hot = torch.cat([true_1_hot_s, true_1_hot_f], dim=1)
+        pos_prob = torch.sigmoid(logits)
+        neg_prob = 1 - pos_prob
+        probas = torch.cat([pos_prob, neg_prob], dim=1)
+    else:
+        true_1_hot = torch.eye(num_classes)[true.squeeze(1)]
+        true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
+        probas = F.softmax(logits, dim=1)
+    true_1_hot = true_1_hot.type(logits.type())
+    dims = (0,) + tuple(range(2, true.ndimension()))
+    intersection = torch.sum(probas * true_1_hot, dims)
+    cardinality = torch.sum(probas + true_1_hot, dims)
+    dice_loss = (2. * intersection / (cardinality + eps)).mean()
+    return (1 - dice_loss)
+
+def ce_loss(true, logits, weights, ignore=255):
+    """Computes the weighted multi-class cross-entropy loss.
+    Args:
+        true: a tensor of shape [B, 1, H, W].
+        logits: a tensor of shape [B, C, H, W]. Corresponds to
+            the raw output or logits of the model.
+        weight: a tensor of shape [C,]. The weights attributed
+            to each class.
+        ignore: the class index to ignore.
+    Returns:
+        ce_loss: the weighted multi-class cross-entropy loss.
+    """
+    ce_loss = F.cross_entropy(
+        logits.float(),
+        true.long(),
+        ignore_index=ignore,
+        weight=weights,
+    )
+    return ce_loss
