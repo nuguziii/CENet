abd9c919e41204ff311ae3a212ec1114ae6fa7c0
CHAEN <nuguziii@naver.com>
2020-05-12 15:30:25 +0900

    edit data_dir option

--- src/train.py
+++ src/train.py
@@ -39,7 +39,7 @@ def train(opt):
     optimizer = optim.Adam(model.parameters(), lr=opt.lr, weight_decay=0.1)
 
     # Data loading code
-    train_dataset = LiverDataset('train')
+    train_dataset = LiverDataset('train', opt.data_dir)
     train_loader = torch.utils.data.DataLoader(
         train_dataset,
         batch_size=opt.batch_size,
