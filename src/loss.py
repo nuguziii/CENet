c5b1dc287e3af80359d4fcdf425e7491c8de8684
CHAEN <nuguziii@naver.com>
2020-05-21 12:02:04 +0900

    delete softdiceloss

--- src/loss.py
+++ src/loss.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
-from torch.autograd import Variable
 
 class Loss(nn.Module):
     def __init__(self):
@@ -15,43 +14,11 @@ class Loss(nn.Module):
         contour_loss = self.ce_loss(contour_gt, contour_output, class_weights)
         return output_loss, shape_loss, contour_loss
 
-class SoftDiceLoss(nn.Module):
-    def __init__(self, n_classes=2):
-        super(SoftDiceLoss, self).__init__()
-        self.one_hot_encoder = One_Hot(n_classes).forward
-        self.n_classes = n_classes
-
-    def forward(self, target, input):
-        smooth = 0.01
-        batch_size = input.size(0)
-        input = input.view(batch_size, self.n_classes, -1)
-        target = self.one_hot_encoder(target).contiguous().view(batch_size, self.n_classes, -1)
-        inter = torch.sum(input * target, 2) + smooth
-        union = torch.sum(input, 2) + torch.sum(target, 2) + smooth
-        score = torch.sum(2.0 * inter / union)
-        score = 1.0 - score / (float(batch_size) * float(self.n_classes))
-        return score
-
-class One_Hot(nn.Module):
-    def __init__(self, depth):
-        super(One_Hot, self).__init__()
-        self.depth = depth
-        self.ones = torch.sparse.torch.eye(depth).cuda()
-    def forward(self, X_in):
-        n_dim = X_in.dim()
-        output_size = X_in.size() + torch.Size([self.depth])
-        num_element = X_in.numel()
-        X_in = X_in.data.long().view(num_element)
-        out = Variable(self.ones.index_select(0, X_in)).view(output_size)
-        return out.permute(0, -1, *range(1, n_dim)).squeeze(dim=2).float()
-    def __repr__(self):
-        return self.__class__.__name__ + "({})".format(self.depth)
-
 class DiceLoss(nn.Module):
     def __init__(self):
         super(DiceLoss, self).__init__()
 
-    def forward(self, target, output, weights=None, ignore_index=None):
+    def forward(self, target, output, weights=None):
         '''
         :param target: NxDxHxW LongTensor
         :param output: NxCxDxHxW Variable
